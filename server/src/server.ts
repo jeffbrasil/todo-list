c630118951f67618fa4aacb800f5ad116efaaf6e
jeffBrasil <jefferson.ribeiro.brasil@ccc.ufcg.edu.br>
2025-03-08 18:55:13 -0300

    Leve alteração no módulo server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -20,6 +20,9 @@ const PORT = process.env.PORT || 5000;
 // Conecta ao MongoDB
 connectDB(); 
 
+
+app.use(express.json());
+
 // Rotas de autenticação
 app.use('/api/auth', authRoutes);
 
@@ -27,7 +30,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes); // Adicione as rotas de tarefas
 
 app.use(cors());
-app.use(express.json());
+
 
 app.get('/', (req, res) => {
   res.send('Backend is running!');
